ed5b23a3d6d2a0d1b8bfa0777c736ed0e5399f76
bosakthi <35524214+bosakthi@users.noreply.github.com>
2018-02-08 16:27:32 +0530

    Update symlink.py

--- symlink.py
+++ symlink.py
@@ -3,14 +3,22 @@
 ##os.symlink('Sakthivel_Boopathi_133479.docx', 'resume')
 ##shutil.move('resume', 'C:/Python27/Assessments/')
 
+#Through Hard Coded Path to create Symbolic links
+##import os
+##src = 'C:\Python27\LICENSE.txt'
+##dst = 'C:\Users\bosakthi\Desktop'
+# This creates a symbolic link on python in tmp directory
+##os.symlink(src, dst)
+##print "symlink created"
+
 
 import os
-
-src = 'C:\Python27\LICENSE.txt'
-dst = 'C:\Users\bosakthi\Desktop'
+# Obtaining the path src file and destination file
+src = input('Enter path of file to create symbolic link: ')
+dst = input("Enter the destination path of the Symbolic file : ")
 
 # This creates a symbolic link on python in tmp directory
 os.symlink(src, dst)
 
-print "symlink created"
+print "symlink created at location : ",dst
 
