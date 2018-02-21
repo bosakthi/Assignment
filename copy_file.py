86afaa0b0d6e6148f21a321a4d5ef8653ff4c079
bosakthi <35524214+bosakthi@users.noreply.github.com>
2018-02-21 16:20:59 +0530

    Add files via upload
    In path give the path of the server where you want to get the files.

---
+++ copy_file.py
@@ -0,0 +1,9 @@
+import os
+import re
+import glob
+
+src = 'src.txt' 
+path = '/user/ec2-user/'#specify the server path
+for infile in glob.glob( os.path.join(path, '*') ):
+    if src.match(infile):
+         print "current file is: " + infile
