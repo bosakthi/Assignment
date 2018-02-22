9fd00adca12fc8592ef1f969db6ca21bc0bb0545
bosakthi <35524214+bosakthi@users.noreply.github.com>
2018-02-22 13:13:31 +0530

    Update copy_file.py
    The above program giving output as,
    Permission denied (publickey).
    lost connection

--- copy_file.py
+++ copy_file.py
@@ -1,9 +1,5 @@
 import os
-import re
-import glob
-
-src = 'src.txt' #specify the file name
-path = '/user/ec2-user/'#specify the server path
-for infile in glob.glob( os.path.join(path, '*') ):
-    if src.match(infile):
-         print "current file is: " + infile
+filePath = "/home/ec2-user/system_info.txt"
+serverPath = "/home/ec2-user/a1/syst.txt"
+os.system("scp "+filePath+" ec2-18-218-23-174.us-east-2.compute.amazonaws.com:"+serverPath)
+print "File Copied Successfully"
